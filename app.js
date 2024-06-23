9552df3a5e229bfc4757b785cf60265d3aaacbe3
Mr.Nobody <id4acc2014@protonmail.com>
2024-06-23 14:24:55 +0330

    feat: :lipstick: Added responsive design for various screen sizes and menu toggle functionality
    Added responsive design for the Crafts Shop Website Design to ensure optimal display across various screen sizes.
    
    Implemented media queries to adjust padding, font sizes, and layout for breakpoints at 1750px, 1400px, 1240px, 1000px, 700px, and 450px.
    
    Added a menu toggle functionality using JavaScript for mobile navigation, allowing the navlist to open and close when the menu icon is clicked.

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
   header.classList.toggle("sticky", window.scrollY > 150);
 });
+
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navlist.classList.remove("open");
+};
